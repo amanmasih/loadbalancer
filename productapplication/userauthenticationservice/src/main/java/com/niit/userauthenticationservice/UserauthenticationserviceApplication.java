07eef765a90875d8480b22cfe6bf89e8532661c5
AMAN MASIH <39672588+amanmasih@users.noreply.github.com>
2022-03-01 17:15:09 +0530

    Add files via upload

---
+++ productapplication/userauthenticationservice/src/main/java/com/niit/userauthenticationservice/UserauthenticationserviceApplication.java
@@ -0,0 +1,16 @@
+package com.niit.userauthenticationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+
+@SpringBootApplication(scanBasePackages="com.niit.userauthenticationservice.controler")
+@EnableEurekaClient
+public class UserauthenticationserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserauthenticationserviceApplication.class, args);
+	}
+
+}
---
+++ productapplication/userproductservice/src/main/java/com/niit/userproductservice/config/BeanConfig.java
@@ -0,0 +1,17 @@
+package com.niit.userproductservice.config;
+
+import com.niit.userproductservice.filter.JwtFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfig {
+    @Bean
+    FilterRegistrationBean jwtFilter(){
+        FilterRegistrationBean filterRegistrationBean=new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new JwtFilter());
+        filterRegistrationBean.addUrlPatterns("/api/v1/customerData/**");
+        return filterRegistrationBean;
+    }
+}
